eac42e01e35886aa371434efbf91bf2abd6a79db
Daniel Bachhuber <daniel@handbuilt.co>
2016-04-08 04:51:17 -0700

    Initial commit

---
+++ wp-saml-auth.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WP SAML Auth
+ * Version: 0.1-alpha
+ * Description: SAML authentication for WordPress, using SimpleSAMLphp.
+ * Author: Pantheon
+ * Author URI: https://pantheon.io
+ * Plugin URI: https://wordpress.org/plugins/wp-saml-auth/
+ * Text Domain: wp-saml-auth
+ * Domain Path: /languages
+ * @package Wp_Saml_Auth
+ */
