c8dfb53a296b64e2f6cf0ac724280df1808c5ff6
Daniel Bachhuber <daniel@handbuilt.co>
2016-04-11 14:07:53 -0700

    http clone instead of Git

--- bin/install-simplesamlphp.sh
+++ bin/install-simplesamlphp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:simplesamlphp/simplesamlphp.git
-cd simplesamlphp;
+git clone https://github.com/simplesamlphp/simplesamlphp.git
+cd simplesamlphp
 composer install
