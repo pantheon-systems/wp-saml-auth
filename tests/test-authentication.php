3b4119d4aef07c78c04f6694bf1f225928046b5a
Daniel Bachhuber <daniel@handbuilt.co>
2016-04-11 12:25:50 -0700

    Rename to `options` for clarity

--- tests/test-authentication.php
+++ tests/test-authentication.php
@@ -5,16 +5,17 @@
  */
 class Test_Authentication extends WP_UnitTestCase {
 
-	private $override = array();
+	private $option = array();
 
 	public function setUp() {
 		parent::setUp();
+		$this->options = array();
 		add_filter( 'wp_saml_auth_option', array( $this, 'filter_wp_saml_auth_option' ), 10, 2 );
 	}
 
 	public function test_user_pass_login_permitted() {
 		$this->factory->user->create( array( 'user_login' => 'testnowplogin', 'user_pass' => 'testnowplogin' ) );
-		$this->override['permit_wp_login'] = true;
+		$this->options['permit_wp_login'] = true;
 		$user = wp_signon( array(
 			'user_login'     => 'testnowplogin',
 			'user_password'  => 'testnowplogin',
@@ -24,7 +25,7 @@ class Test_Authentication extends WP_UnitTestCase {
 
 	public function test_user_pass_login_not_permitted() {
 		$this->factory->user->create( array( 'user_login' => 'testnowplogin', 'user_pass' => 'testnowplogin' ) );
-		$this->override['permit_wp_login'] = false;
+		$this->options['permit_wp_login'] = false;
 		$user = wp_signon( array(
 			'user_login'     => 'testnowplogin',
 			'user_password'  => 'testnowplogin',
@@ -33,14 +34,13 @@ class Test_Authentication extends WP_UnitTestCase {
 	}
 
 	public function filter_wp_saml_auth_option( $value, $option_name ) {
-		if ( isset( $this->override[ $option_name ] ) ) {
-			return $this->override[ $option_name ];
+		if ( isset( $this->options[ $option_name ] ) ) {
+			return $this->options[ $option_name ];
 		}
 		return $value;
 	}
 
 	public function tearDown() {
-		$this->override = array();
 		remove_filter( 'wp_saml_auth_option', array( $this, 'filter_wp_saml_auth_option' ) );
 		parent::tearDown();
 	}
