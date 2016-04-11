9f2a578e2e2b191196c724829e41a6639b747792
Daniel Bachhuber <daniel@handbuilt.co>
2016-04-11 12:22:19 -0700

    Add a test for logging in / out via password

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,8 +18,23 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/wp-saml-auth.php';
+
+	add_filter( 'wp_saml_auth_option', '_wp_saml_auth_filter_option', 10, 2 );
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+function _wp_saml_auth_filter_option( $value, $option_name ) {
+	switch ( $option_name ) {
+		case 'simplesamlphp_autoload':
+			$value = dirname( __FILE__ ) . '/class-simplesaml-auth-simple.php';
+			break;
+	}
+	return $value;
+}
+
+function wp_set_auth_cookie( $user_id, $remember = false, $secure = '', $token = '' ) {
+	return true;
+}
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
