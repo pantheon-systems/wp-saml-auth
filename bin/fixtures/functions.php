7d16baabe246528a7bf3252f79dec7fa1f7230b6
Daniel Bachhuber <daniel@handbuilt.co>
2016-06-29 09:53:36 -0700

    Ensure child theme enqueues the styles

--- bin/fixtures/functions.php
+++ bin/fixtures/functions.php
@@ -1,5 +1,17 @@
 <?php
 
+add_action( 'wp_enqueue_scripts', 'samltheme_parent_theme_enqueue_styles' );
+
+function samltheme_parent_theme_enqueue_styles() {
+    wp_enqueue_style( 'twentysixteen-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'samltheme-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'twentysixteen-style' )
+    );
+
+}
+
+
 function samltheme_filter_option( $value, $option_name ) {
     $defaults = array(
         /**
