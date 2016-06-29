fbd16510cfee374d4861eda7928d2923fd908c4b
Daniel Bachhuber <daniel@handbuilt.co>
2016-06-29 16:26:52 -0700

    Symlink name was changed in f71194d85076aab03e3acb3d0f93c9774b9cb709

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -82,7 +82,7 @@ ln -s ./private/simplesamlphp/www ./simplesaml
 # Push files to the environment
 ###
 cd $PREPARE_DIR
-git add private wp-content simplesamlphp
+git add private wp-content simplesaml
 git config user.email "wp-saml-auth@getpantheon.com"
 git config user.name "Pantheon"
 git commit -m "Include SimpleSAML PHP and its configuration files"
