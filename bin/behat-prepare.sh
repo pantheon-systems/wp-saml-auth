56f57657a0faf6e08236e7706eb738f2d6b82836
Steve Persch <stevector@users.noreply.github.com>
2016-07-28 15:50:37 -0500

    switching download url to github
    Comment out cleanup
    
    cleanup

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -70,7 +70,7 @@ rm -rf $PREPARE_DIR/wp-content/plugins/wp-saml-auth/.git
 ###
 rm -rf $PREPARE_DIR/private
 mkdir $PREPARE_DIR/private
-wget -O $PREPARE_DIR/simplesamlphp.tar.gz https://simplesamlphp.org/res/downloads/simplesamlphp-$SIMPLESAMLPHP_VERSION.tar.gz
+wget -O $PREPARE_DIR/simplesamlphp.tar.gz https://github.com/simplesamlphp/simplesamlphp/releases/download/v$SIMPLESAMLPHP_VERSION/simplesamlphp-$SIMPLESAMLPHP_VERSION.tar.gz
 tar -zxvf $PREPARE_DIR/simplesamlphp.tar.gz -C $PREPARE_DIR/private
 mv $PREPARE_DIR/private/simplesamlphp-$SIMPLESAMLPHP_VERSION $PREPARE_DIR/private/simplesamlphp
 rm $PREPARE_DIR/simplesamlphp.tar.gz
