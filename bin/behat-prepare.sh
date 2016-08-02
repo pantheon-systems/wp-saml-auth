8ec819ba672c322c3241a621d6371a94bc6a8b12
Steve Persch <steve.persch@pantheon.io>
2016-08-02 18:24:09 -0500

    Supress admin pw

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -118,6 +118,9 @@ sleep 10
 ###
 # Set up WordPress, theme, and plugins for the test run
 ###
-terminus wp "core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=pantheon --admin_email=wp-saml-auth@getpantheon.com --admin_password=pantheon"
+# Silence output so as not to show the password.
+{
+  terminus wp "core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=$WORDPRESS_ADMIN_USERNAME --admin_email=no-reply@getpantheon.com --admin_password=$WORDPRESS_ADMIN_PASSWORD"
+} &> /dev/null
 terminus wp "plugin activate wp-native-php-sessions wp-saml-auth"
 terminus wp "theme activate $TERMINUS_SITE"
