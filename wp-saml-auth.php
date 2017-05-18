975d8ed04120e1bd3d32ff293531286a3e04830a
audiovisuel-uqam <audiovisueluqam@gmail.com>
2017-05-18 17:11:12 -0400

    Unless user is admin, password cannot be changed
    Unless user is admin, password cannot be changed

--- wp-saml-auth.php
+++ wp-saml-auth.php
@@ -121,3 +121,35 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once dirname( __FILE__ ) . '/inc/class-wp-saml-auth-cli.php';
 	WP_CLI::add_command( 'saml-auth', 'WP_SAML_Auth_CLI' );
 }
+
+/**
+ * Password_Reset_Removed.
+ */
+class Password_Reset_Removed
+{
+
+    function __construct()
+    {
+        add_filter( 'show_password_fields', array( $this, 'disable' ) );
+        add_filter( 'allow_password_reset', array( $this, 'disable' ) );
+        add_filter( 'gettext',              array( $this, 'remove' ) );
+    }
+
+    function disable()
+    {
+        if ( is_admin() ) {
+            $userdata = wp_get_current_user();
+            $user = new WP_User($userdata->ID);
+            if ( !empty( $user->roles ) && is_array( $user->roles ) && $user->roles[0] == 'administrator' )
+                return true;
+        }
+        return false;
+    }
+
+    function remove($text)
+    {
+        return str_replace( array('Lost your password?', 'Lost your password'), '', trim($text, '?') );
+    }
+}
+
+$pass_reset_removed = new Password_Reset_Removed();
