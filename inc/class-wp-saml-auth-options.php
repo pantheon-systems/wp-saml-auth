b3e1316061831372d55639a77c5756995debfd2a
Scott McKenzie <noizyland@gmail.com>
2019-04-27 00:00:50 +1000

    Convert spaces back to tabs.

--- inc/class-wp-saml-auth-options.php
+++ inc/class-wp-saml-auth-options.php
@@ -10,80 +10,80 @@
  */
 class WP_SAML_Auth_Options {
 
-    /**
-     * Controller instance as a singleton
-     *
-     * @var object
-     */
-    private static $instance;
+	/**
+	 * Controller instance as a singleton
+	 *
+	 * @var object
+	 */
+	private static $instance;
 
-    /**
-     * Wordpress options name
-     *
-     * @var string
-     */
-    private static $option_name = 'wp-saml-auth-settings';
+	/**
+	 * WordPress options name
+	 *
+	 * @var string
+	 */
+	private static $option_name = 'wp-saml-auth-settings';
 
-    /**
-     * Wordpress options array cache
-     *
-     * @var array
-     */
-    private static $options;
+	/**
+	 * WordPress options array cache
+	 *
+	 * @var array
+	 */
+	private static $options;
 
-    /**
-     * Get the controller instance
-     *
-     * @return object
-     */
-    public static function get_instance() {
-        if ( ! isset( self::$instance ) ) {
-            self::$instance = new WP_SAML_Auth_Options;
+	/**
+	 * Get the controller instance
+	 *
+	 * @return object
+	 */
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new WP_SAML_Auth_Options;
 
-            self::$options = get_option( self::$option_name );
-            if ( isset( self::$options['connection_type'] ) && 'internal' === self::$options['connection_type'] ) {
-                add_filter( 'wp_saml_auth_option', array( self::$instance, 'filter_option' ), 10, 2 );
-            }
-        }
-        return self::$instance;
-    }
+			self::$options = get_option( self::$option_name );
+			if ( isset( self::$options['connection_type'] ) && 'internal' === self::$options['connection_type'] ) {
+				add_filter( 'wp_saml_auth_option', array( self::$instance, 'filter_option' ), 10, 2 );
+			}
+		}
+		return self::$instance;
+	}
 
-    /**
-     * Options for WP SAML Auth loaded from database.
-     *
-     * @param mixed  $value       Configuration value.
-     * @param string $option_name Configuration option name.
-     */
-    public static function filter_option( $value, $option_name ) {
-        $defaults = array(
-            'internal_config' => array(
-                'strict'  => true,
-                'debug'   => defined( 'WP_DEBUG' ) && WP_DEBUG ? true : false,
-                'baseurl' => self::$options['baseurl'],
-                'sp'      => array(
-                    'entityId'                 => self::$options['sp_entityId'],
-                    'assertionConsumerService' => array(
-                        'url'     => self::$options['sp_assertionConsumerService_url'],
-                        'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-                    ),
-                ),
-                'idp'     => array(
-                    'entityId'                 => self::$options['idp_entityId'],
-                    'singleSignOnService'      => array(
-                        'url'     => self::$options['idp_singleSignOnService_url'],
-                        'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-                    ),
-                    'singleLogoutService'      => array(
-                        'url'     => self::$options['idp_singleLogoutService_url'],
-                        'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-                    ),
-                    'certFingerprint'          => self::$options['certFingerprint'],
-                    'certFingerprintAlgorithm' => self::$options['certFingerprintAlgorithm'],
-                ),
-            ),
-            'default_role'    => get_option( 'default_role' ),
-        );
-        $value = isset( self::$options[ $option_name ] ) ? self::$options[ $option_name ] : $defaults[ $option_name ];
-        return $value;
-    }
+	/**
+	 * Options for WP SAML Auth loaded from database.
+	 *
+	 * @param mixed  $value       Configuration value.
+	 * @param string $option_name Configuration option name.
+	 */
+	public static function filter_option( $value, $option_name ) {
+		$defaults = array(
+			'internal_config' => array(
+				'strict'  => true,
+				'debug'   => defined( 'WP_DEBUG' ) && WP_DEBUG ? true : false,
+				'baseurl' => self::$options['baseurl'],
+				'sp'      => array(
+					'entityId'                 => self::$options['sp_entityId'],
+					'assertionConsumerService' => array(
+						'url'     => self::$options['sp_assertionConsumerService_url'],
+						'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+					),
+				),
+				'idp'     => array(
+					'entityId'                 => self::$options['idp_entityId'],
+					'singleSignOnService'      => array(
+						'url'     => self::$options['idp_singleSignOnService_url'],
+						'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+					),
+					'singleLogoutService'      => array(
+						'url'     => self::$options['idp_singleLogoutService_url'],
+						'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+					),
+					'certFingerprint'          => self::$options['certFingerprint'],
+					'certFingerprintAlgorithm' => self::$options['certFingerprintAlgorithm'],
+				),
+			),
+			'default_role'    => get_option( 'default_role' ),
+		);
+		$value = isset( self::$options[ $option_name ] ) ? self::$options[ $option_name ] : $defaults[ $option_name ];
+		return $value;
+	}
 }
--- wp-saml-auth.php
+++ wp-saml-auth.php
@@ -19,148 +19,148 @@
  * @param string $option_name Configuration option name.
  */
 function wpsa_filter_option( $value, $option_name ) {
-    $defaults = array(
-        /**
-         * Type of SAML connection bridge to use.
-         *
-         * 'internal' uses OneLogin bundled library; 'simplesamlphp' uses SimpleSAMLphp.
-         *
-         * Defaults to SimpleSAMLphp for backwards compatibility.
-         *
-         * @param string
-         */
-        'connection_type'        => 'simplesamlphp',
-        /**
-         * Path to SimpleSAMLphp autoloader.
-         *
-         * Follow the standard implementation by installing SimpleSAMLphp
-         * alongside the plugin, and provide the path to its autoloader.
-         * Alternatively, this plugin will work if it can find the
-         * `SimpleSAML_Auth_Simple` class.
-         *
-         * @param string
-         */
-        'simplesamlphp_autoload' => dirname( __FILE__ ) . '/simplesamlphp/lib/_autoload.php',
-        /**
-         * Authentication source to pass to SimpleSAMLphp
-         *
-         * This must be one of your configured identity providers in
-         * SimpleSAMLphp. If the identity provider isn't configured
-         * properly, the plugin will not work properly.
-         *
-         * @param string
-         */
-        'auth_source'            => 'default-sp',
-        /**
-         * Configuration options for OneLogin library use.
-         *
-         * See comments with "Required:" for values you absolutely need to configure.
-         *
-         * @param array
-         */
-        'internal_config'        => array(
-            // Validation of SAML responses is required.
-            'strict'  => true,
-            'debug'   => defined( 'WP_DEBUG' ) && WP_DEBUG ? true : false,
-            'baseurl' => home_url(),
-            'sp'      => array(
-                'entityId'                 => 'urn:' . parse_url( home_url(), PHP_URL_HOST ),
-                'assertionConsumerService' => array(
-                    'url'     => home_url(),
-                    'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-                ),
-            ),
-            'idp'     => array(
-                // Required: Set based on provider's supplied value.
-                'entityId'                 => '',
-                'singleSignOnService'      => array(
-                    // Required: Set based on provider's supplied value.
-                    'url'     => '',
-                    'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-                ),
-                'singleLogoutService'      => array(
-                    // Required: Set based on provider's supplied value.
-                    'url'     => '',
-                    'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-                ),
-                // Required: Contents of the IDP's public x509 certificate.
-                // Use file_get_contents() to load certificate contents into scope.
-                'x509cert'                 => '',
-                // Optional: Instead of using the x509 cert, you can specify the fingerprint and algorithm.
-                'certFingerprint'          => '',
-                'certFingerprintAlgorithm' => '',
-            ),
-        ),
-        /**
-         * Whether or not to automatically provision new WordPress users.
-         *
-         * When WordPress is presented with a SAML user without a
-         * corresponding WordPress account, it can either create a new user
-         * or display an error that the user needs to contact the site
-         * administrator.
-         *
-         * @param bool
-         */
-        'auto_provision'         => true,
-        /**
-         * Whether or not to permit logging in with username and password.
-         *
-         * If this feature is disabled, all authentication requests will be
-         * channeled through SimpleSAMLphp.
-         *
-         * @param bool
-         */
-        'permit_wp_login'        => true,
-        /**
-         * Attribute by which to get a WordPress user for a SAML user.
-         *
-         * @param string Supported options are 'email' and 'login'.
-         */
-        'get_user_by'            => 'email',
-        /**
-         * SAML attribute which includes the user_login value for a user.
-         *
-         * @param string
-         */
-        'user_login_attribute'   => 'uid',
-        /**
-         * SAML attribute which includes the user_email value for a user.
-         *
-         * @param string
-         */
-        'user_email_attribute'   => 'mail',
-        /**
-         * SAML attribute which includes the display_name value for a user.
-         *
-         * @param string
-         */
-        'display_name_attribute' => 'display_name',
-        /**
-         * SAML attribute which includes the first_name value for a user.
-         *
-         * @param string
-         */
-        'first_name_attribute'   => 'first_name',
-        /**
-         * SAML attribute which includes the last_name value for a user.
-         *
-         * @param string
-         */
-        'last_name_attribute'    => 'last_name',
-        /**
-         * Default WordPress role to grant when provisioning new users.
-         *
-         * @param string
-         */
-        'default_role'           => get_option( 'default_role' ),
-    );
-    $value = isset( $defaults[ $option_name ] ) ? $defaults[ $option_name ] : $value;
-    return $value;
+	$defaults = array(
+		/**
+		 * Type of SAML connection bridge to use.
+		 *
+		 * 'internal' uses OneLogin bundled library; 'simplesamlphp' uses SimpleSAMLphp.
+		 *
+		 * Defaults to SimpleSAMLphp for backwards compatibility.
+		 *
+		 * @param string
+		 */
+		'connection_type'        => 'simplesamlphp',
+		/**
+		 * Path to SimpleSAMLphp autoloader.
+		 *
+		 * Follow the standard implementation by installing SimpleSAMLphp
+		 * alongside the plugin, and provide the path to its autoloader.
+		 * Alternatively, this plugin will work if it can find the
+		 * `SimpleSAML_Auth_Simple` class.
+		 *
+		 * @param string
+		 */
+		'simplesamlphp_autoload' => dirname( __FILE__ ) . '/simplesamlphp/lib/_autoload.php',
+		/**
+		 * Authentication source to pass to SimpleSAMLphp
+		 *
+		 * This must be one of your configured identity providers in
+		 * SimpleSAMLphp. If the identity provider isn't configured
+		 * properly, the plugin will not work properly.
+		 *
+		 * @param string
+		 */
+		'auth_source'            => 'default-sp',
+		/**
+		 * Configuration options for OneLogin library use.
+		 *
+		 * See comments with "Required:" for values you absolutely need to configure.
+		 *
+		 * @param array
+		 */
+		'internal_config'        => array(
+			// Validation of SAML responses is required.
+			'strict'  => true,
+			'debug'   => defined( 'WP_DEBUG' ) && WP_DEBUG ? true : false,
+			'baseurl' => home_url(),
+			'sp'      => array(
+				'entityId'                 => 'urn:' . parse_url( home_url(), PHP_URL_HOST ),
+				'assertionConsumerService' => array(
+					'url'     => home_url(),
+					'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+				),
+			),
+			'idp'     => array(
+				// Required: Set based on provider's supplied value.
+				'entityId'                 => '',
+				'singleSignOnService'      => array(
+					// Required: Set based on provider's supplied value.
+					'url'     => '',
+					'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+				),
+				'singleLogoutService'      => array(
+					// Required: Set based on provider's supplied value.
+					'url'     => '',
+					'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+				),
+				// Required: Contents of the IDP's public x509 certificate.
+				// Use file_get_contents() to load certificate contents into scope.
+				'x509cert'                 => '',
+				// Optional: Instead of using the x509 cert, you can specify the fingerprint and algorithm.
+				'certFingerprint'          => '',
+				'certFingerprintAlgorithm' => '',
+			),
+		),
+		/**
+		 * Whether or not to automatically provision new WordPress users.
+		 *
+		 * When WordPress is presented with a SAML user without a
+		 * corresponding WordPress account, it can either create a new user
+		 * or display an error that the user needs to contact the site
+		 * administrator.
+		 *
+		 * @param bool
+		 */
+		'auto_provision'         => true,
+		/**
+		 * Whether or not to permit logging in with username and password.
+		 *
+		 * If this feature is disabled, all authentication requests will be
+		 * channeled through SimpleSAMLphp.
+		 *
+		 * @param bool
+		 */
+		'permit_wp_login'        => true,
+		/**
+		 * Attribute by which to get a WordPress user for a SAML user.
+		 *
+		 * @param string Supported options are 'email' and 'login'.
+		 */
+		'get_user_by'            => 'email',
+		/**
+		 * SAML attribute which includes the user_login value for a user.
+		 *
+		 * @param string
+		 */
+		'user_login_attribute'   => 'uid',
+		/**
+		 * SAML attribute which includes the user_email value for a user.
+		 *
+		 * @param string
+		 */
+		'user_email_attribute'   => 'mail',
+		/**
+		 * SAML attribute which includes the display_name value for a user.
+		 *
+		 * @param string
+		 */
+		'display_name_attribute' => 'display_name',
+		/**
+		 * SAML attribute which includes the first_name value for a user.
+		 *
+		 * @param string
+		 */
+		'first_name_attribute'   => 'first_name',
+		/**
+		 * SAML attribute which includes the last_name value for a user.
+		 *
+		 * @param string
+		 */
+		'last_name_attribute'    => 'last_name',
+		/**
+		 * Default WordPress role to grant when provisioning new users.
+		 *
+		 * @param string
+		 */
+		'default_role'           => get_option( 'default_role' ),
+	);
+	$value = isset( $defaults[ $option_name ] ) ? $defaults[ $option_name ] : $value;
+	return $value;
 }
 add_filter( 'wp_saml_auth_option', 'wpsa_filter_option', 0, 2 );
 
 if ( ! defined( 'WP_SAML_AUTH_AUTOLOADER' ) ) {
-    define( 'WP_SAML_AUTH_AUTOLOADER', __DIR__ . '/vendor/autoload.php' );
+	define( 'WP_SAML_AUTH_AUTOLOADER', __DIR__ . '/vendor/autoload.php' );
 }
 
 /**
@@ -172,17 +172,17 @@ require_once dirname( __FILE__ ) . '/inc/class-wp-saml-auth.php';
 WP_SAML_Auth::get_instance();
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-    require_once dirname( __FILE__ ) . '/inc/class-wp-saml-auth-cli.php';
-    WP_CLI::add_command( 'saml-auth', 'WP_SAML_Auth_CLI' );
+	require_once dirname( __FILE__ ) . '/inc/class-wp-saml-auth-cli.php';
+	WP_CLI::add_command( 'saml-auth', 'WP_SAML_Auth_CLI' );
 }
 
 /**
  * Initialize the WP SAML Auth plugin settings page.
  */
 if ( is_admin() ) {
-    require_once dirname( __FILE__ ) . '/inc/class-wp-saml-auth-settings.php';
-    WP_SAML_Auth_Settings::get_instance();
-    register_uninstall_hook( __FILE__, 'uninstall_hook' );
+	require_once dirname( __FILE__ ) . '/inc/class-wp-saml-auth-settings.php';
+	WP_SAML_Auth_Settings::get_instance();
+	register_uninstall_hook( __FILE__, 'uninstall_hook' );
 }
 
 /**
@@ -195,6 +195,6 @@ WP_SAML_Auth_Options::get_instance();
  * Delete option from DB on uninstallation.
  */
 function uninstall_hook() {
-    $settings = WP_SAML_Auth_Settings::get_instance();
-    delete_option( $settings->option_name() );
+	$settings = WP_SAML_Auth_Settings::get_instance();
+	delete_option( $settings->option_name() );
 }
