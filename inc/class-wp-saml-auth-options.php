5a905b2d7615fcff1c941f155db231ee48397588
Daniel Bachhuber <daniel@bachhuber.co>
2019-05-14 06:15:52 -0700

    Avoid options static cache

--- inc/class-wp-saml-auth-options.php
+++ inc/class-wp-saml-auth-options.php
@@ -25,13 +25,6 @@ class WP_SAML_Auth_Options {
 	private static $option_name = 'wp-saml-auth-settings';
 
 	/**
-	 * WordPress options array cache
-	 *
-	 * @var array
-	 */
-	private static $options;
-
-	/**
 	 * Get the controller instance
 	 *
 	 * @return object
@@ -40,8 +33,8 @@ class WP_SAML_Auth_Options {
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new WP_SAML_Auth_Options;
 
-			self::$options = get_option( self::$option_name );
-			if ( isset( self::$options['connection_type'] ) && 'internal' === self::$options['connection_type'] ) {
+			$options = get_option( self::$option_name );
+			if ( isset( $options['connection_type'] ) && 'internal' === $options['connection_type'] ) {
 				add_filter( 'wp_saml_auth_option', array( self::$instance, 'filter_option' ), 10, 2 );
 			}
 		}
@@ -55,35 +48,36 @@ class WP_SAML_Auth_Options {
 	 * @param string $option_name Configuration option name.
 	 */
 	public static function filter_option( $value, $option_name ) {
+		$options  = get_option( self::$option_name );
 		$defaults = array(
 			'internal_config' => array(
 				'strict'  => true,
 				'debug'   => defined( 'WP_DEBUG' ) && WP_DEBUG ? true : false,
-				'baseurl' => self::$options['baseurl'],
+				'baseurl' => $options['baseurl'],
 				'sp'      => array(
-					'entityId'                 => self::$options['sp_entityId'],
+					'entityId'                 => $options['sp_entityId'],
 					'assertionConsumerService' => array(
-						'url'     => self::$options['sp_assertionConsumerService_url'],
+						'url'     => $options['sp_assertionConsumerService_url'],
 						'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
 					),
 				),
 				'idp'     => array(
-					'entityId'                 => self::$options['idp_entityId'],
+					'entityId'                 => $options['idp_entityId'],
 					'singleSignOnService'      => array(
-						'url'     => self::$options['idp_singleSignOnService_url'],
+						'url'     => $options['idp_singleSignOnService_url'],
 						'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
 					),
 					'singleLogoutService'      => array(
-						'url'     => self::$options['idp_singleLogoutService_url'],
+						'url'     => $options['idp_singleLogoutService_url'],
 						'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
 					),
-					'certFingerprint'          => self::$options['certFingerprint'],
-					'certFingerprintAlgorithm' => self::$options['certFingerprintAlgorithm'],
+					'certFingerprint'          => $options['certFingerprint'],
+					'certFingerprintAlgorithm' => $options['certFingerprintAlgorithm'],
 				),
 			),
 			'default_role'    => get_option( 'default_role' ),
 		);
-		$value    = isset( self::$options[ $option_name ] ) ? self::$options[ $option_name ] : $defaults[ $option_name ];
+		$value    = isset( $options[ $option_name ] ) ? $options[ $option_name ] : $defaults[ $option_name ];
 		return $value;
 	}
 }
