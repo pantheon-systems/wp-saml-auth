d3b1a57c0c3436e105cf7115f2a906ae1f111845
Daniel Bachhuber <daniel@handbuilt.co>
2019-11-07 10:18:30 -0800

    Update README and POT for v0.7.3

--- wp-saml-auth.php
+++ wp-saml-auth.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP SAML Auth
- * Version: 0.7.2
+ * Version: 0.7.3
  * Description: SAML authentication for WordPress, using SimpleSAMLphp.
  * Author: Pantheon
  * Author URI: https://pantheon.io
