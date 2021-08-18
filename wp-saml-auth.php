7bc1819e7d1a583220fc14b19b38c659df6c6f93
Daniel Bachhuber <daniel@bachhuber.co>
2021-08-18 14:32:40 -0700

    Update README for v1.2.5

--- wp-saml-auth.php
+++ wp-saml-auth.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP SAML Auth
- * Version: 1.2.4
+ * Version: 1.2.5
  * Description: SAML authentication for WordPress, using SimpleSAMLphp.
  * Author: Pantheon
  * Author URI: https://pantheon.io
