a8aa12f4e96928850d7cfad00b4b39e598a381c8
Chris Reynolds <chris@jazzsequence.com>
2025-05-14 08:42:37 -0600

    replace .additions files with actual configuration files we copy

---
+++ bin/fixtures/config.php
@@ -0,0 +1,23 @@
+<?php
+
+$config = [];
+
+$config['secretsalt'] = 'todo';
+$config['auth.adminpassword'] = 'admin';
+$config['technicalcontact_email'] = 'test@example.com';
+
+$config['baseurlpath'] = '/simplesaml/';
+$config['certdir'] = 'cert/';
+$config['loggingdir'] = $_SERVER['HOME'] . '/files/simplesaml/log/';
+$config['datadir'] = $_SERVER['HOME'] . '/files/simplesaml/data/';
+$config['tempdir'] = '/srv/bindings/' . $_ENV['PANTHEON_BINDING'] . '/tmp/simplesaml';
+
+$config['tempdir'] = $_SERVER['HOME'] . '/tmp/simplesaml';
+
+$config['store.type'] = 'sql';
+$config['store.sql.dsn'] = 'sqlite:/tmp/sqlitedatabase.sq3';
+
+$config['enable.saml20-idp'] = true;
+$config['enable.shib13-idp'] = true;
+
+return $config;
