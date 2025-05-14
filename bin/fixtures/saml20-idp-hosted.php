3c7ed1c756187e4189b5101d07076c8a48a8b8c3
Chris Reynolds <chris@jazzsequence.com>
2025-05-14 09:21:22 -0600

    change DYNAMIC:1 to DEFAULT for idp metadata

--- bin/fixtures/saml20-idp-hosted.php
+++ bin/fixtures/saml20-idp-hosted.php
@@ -5,7 +5,7 @@
  * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-hosted
  */
 
-$metadata['__DYNAMIC:1__'] = array(
+$metadata['__DEFAULT__'] = array(
 	/*
 	 * The hostname of the server (VHOST) that will use this SAML entity.
 	 *
