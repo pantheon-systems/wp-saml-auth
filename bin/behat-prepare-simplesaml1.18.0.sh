2d20425076afa35a5286035571452cb6155241dc
Chris Reynolds <chris@jazzsequence.com>
2025-05-20 08:22:36 -0600

    re-add files for 1.18 version of simplesaml

--- bin/behat-prepare-simplesaml1.18.0.sh
+++ bin/behat-prepare-simplesaml1.18.0.sh
@@ -22,13 +22,13 @@ fi
 ###
 # Create a new environment for this particular test run.
 ###
-terminus env:create $TERMINUS_SITE.dev $TERMINUS_ENV
-terminus env:wipe $SITE_ENV --yes
+terminus env:create "$TERMINUS_SITE".dev "$TERMINUS_ENV"
+terminus env:wipe "$SITE_ENV" --yes
 
 ###
 # Get all necessary environment details.
 ###
-PANTHEON_GIT_URL=$(terminus connection:info $SITE_ENV --field=git_url)
+PANTHEON_GIT_URL=$(terminus connection:info "$SITE_ENV" --field=git_url)
 PANTHEON_SITE_URL="$TERMINUS_ENV-$TERMINUS_SITE.pantheonsite.io"
 PREPARE_DIR="/tmp/$TERMINUS_ENV-$TERMINUS_SITE"
 BASH_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -44,27 +44,27 @@ git clone -b $TERMINUS_ENV $PANTHEON_GIT_URL $PREPARE_DIR
 # Add WP Native PHP Sessions and child theme to environment
 ###
 echo "Creating a child theme called $TERMINUS_SITE"
-rm -rf $PREPARE_DIR/wp-content/themes/$TERMINUS_SITE
+rm -rf "$PREPARE_DIR"/wp-content/themes/"$TERMINUS_SITE"
 # Create a child theme that includes WP SAML Auth configuration details
-mkdir $PREPARE_DIR/wp-content/themes/$TERMINUS_SITE
-cp $BASH_DIR/fixtures/functions.php  $PREPARE_DIR/wp-content/themes/$TERMINUS_SITE/functions.php
-cp $BASH_DIR/fixtures/style.css  $PREPARE_DIR/wp-content/themes/$TERMINUS_SITE/style.css
+mkdir "$PREPARE_DIR"/wp-content/themes/"$TERMINUS_SITE"
+cp $"BASH_DIR"/fixtures/functions.simplesaml1.18.0.php  "$PREPARE_DIR"/wp-content/themes/$TERMINUS_SITE/functions.php
+cp $"BASH_DIR"/fixtures/style.css  "$PREPARE_DIR"/wp-content/themes/$TERMINUS_SITE/style.css
 
 echo "Adding WP Native PHP Sessions to the environment"
-rm -rf $PREPARE_DIR/wp-content/plugins/wp-native-php-sessions
+rm -rf "$PREPARE_DIR"/wp-content/plugins/wp-native-php-sessions
 # Download the latest WP Native PHP sessions release from WordPress.org
-wget -O $PREPARE_DIR/wp-native-php-sessions.zip https://downloads.wordpress.org/plugin/wp-native-php-sessions.zip
-unzip $PREPARE_DIR/wp-native-php-sessions.zip -d $PREPARE_DIR
-mv $PREPARE_DIR/wp-native-php-sessions $PREPARE_DIR/wp-content/plugins/
-rm $PREPARE_DIR/wp-native-php-sessions.zip
+wget -O "$PREPARE_DIR"/wp-native-php-sessions.zip https://downloads.wordpress.org/plugin/wp-native-php-sessions.zip
+unzip "$PREPARE_DIR"/wp-native-php-sessions.zip -d "$PREPARE_DIR"
+mv "$PREPARE_DIR"/wp-native-php-sessions "$PREPARE_DIR"/wp-content/plugins/
+rm "$PREPARE_DIR"/wp-native-php-sessions.zip
 
 ###
 # Add the copy of this plugin itself to the environment
 ###
 echo "Copying WP SAML Auth into WordPress"
-cd $BASH_DIR/..
-rsync -av --exclude='node_modules/' --exclude='simplesamlphp/' --exclude='tests/' ./* $PREPARE_DIR/wp-content/plugins/wp-saml-auth
-rm -rf $PREPARE_DIR/wp-content/plugins/wp-saml-auth/.git
+cd "$BASH_DIR"/..
+rsync -av --exclude='node_modules/' --exclude='simplesamlphp/' --exclude='tests/' ./* "$PREPARE_DIR"/wp-content/plugins/wp-saml-auth
+rm -rf "$PREPARE_DIR"/wp-content/plugins/wp-saml-auth/.git
 
 ###
 # Add SimpleSAMLphp to the environment
@@ -72,13 +72,13 @@ rm -rf $PREPARE_DIR/wp-content/plugins/wp-saml-auth/.git
 # web root
 ###
 echo "Setting up SimpleSAMLphp"
-rm -rf $PREPARE_DIR/private
-mkdir $PREPARE_DIR/private
-wget https://github.com/simplesamlphp/simplesamlphp/releases/download/v1.18.4/simplesamlphp-1.18.4.tar.gz -O $PREPARE_DIR/simplesamlphp-latest.tar.gz
-tar -zxvf $PREPARE_DIR/simplesamlphp-latest.tar.gz -C $PREPARE_DIR/private
-ORIG_SIMPLESAMLPHP_DIR=$(ls $PREPARE_DIR/private)
-mv $PREPARE_DIR/private/$ORIG_SIMPLESAMLPHP_DIR $PREPARE_DIR/private/simplesamlphp
-rm $PREPARE_DIR/simplesamlphp-latest.tar.gz
+rm -rf "$PREPARE_DIR"/private
+mkdir "$PREPARE_DIR"/private
+wget https://github.com/simplesamlphp/simplesamlphp/releases/download/v1.18.4/simplesamlphp-1.18.4.tar.gz -O "$PREPARE_DIR"/simplesamlphp-latest.tar.gz
+tar -zxvf "$PREPARE_DIR"/simplesamlphp-latest.tar.gz -C "$PREPARE_DIR"/private
+ORIG_SIMPLESAMLPHP_DIR=$(ls "$PREPARE_DIR"/private)
+mv "$PREPARE_DIR"/private/$ORIG_SIMPLESAMLPHP_DIR "$PREPARE_DIR"/private/simplesamlphp
+rm "$PREPARE_DIR"/simplesamlphp-latest.tar.gz
 
 ###
 # Configure SimpleSAMLphp for the environment
@@ -86,43 +86,42 @@ rm $PREPARE_DIR/simplesamlphp-latest.tar.gz
 # provider with its exampleauth module enabled
 ###
 # Append existing configuration files with our the specifics for our tests
-echo "// This variable was added by behat-prepare.sh." >>  $PREPARE_DIR/private/simplesamlphp/config/authsources.php
+echo "// This variable was added by behat-prepare.sh." >>  "$PREPARE_DIR"/private/simplesamlphp/config/authsources.php
 # Silence output so as not to show the password.
 {
-  echo "\$wordpress_admin_password = '"${WORDPRESS_ADMIN_PASSWORD}"';" >> $PREPARE_DIR/private/simplesamlphp/config/authsources.php
+  echo "\$wordpress_admin_password = '"${WORDPRESS_ADMIN_PASSWORD}"';" >> "$PREPARE_DIR"/private/simplesamlphp/config/authsources.php
 } &> /dev/null
-echo "\$wordpress_admin_username = '"${WORDPRESS_ADMIN_USERNAME}"';" >> $PREPARE_DIR/private/simplesamlphp/config/authsources.php
-echo "\$wordpress_admin_email = '"${WORDPRESS_ADMIN_EMAIL}"';" >> $PREPARE_DIR/private/simplesamlphp/config/authsources.php
-cat $BASH_DIR/fixtures/authsources.php.additions >> $PREPARE_DIR/private/simplesamlphp/config/authsources.php
-cat $BASH_DIR/fixtures/config.php.additions      >> $PREPARE_DIR/private/simplesamlphp/config/config.php
+echo "\$wordpress_admin_username = '${WORDPRESS_ADMIN_USERNAME}';" >> "$PREPARE_DIR"/private/simplesamlphp/config/authsources.php
+echo "\$wordpress_admin_email = '${WORDPRESS_ADMIN_EMAIL}';" >> "$PREPARE_DIR"/private/simplesamlphp/config/authsources.php
+cat "$BASH_DIR"/fixtures/authsources.php.additions >> "$PREPARE_DIR"/private/simplesamlphp/config/authsources.php
+cat "$BASH_DIR"/fixtures/config.php.additions      >> "$PREPARE_DIR"/private/simplesamlphp/config/config.php
 
 # Copy identify provider configuration files into their appropriate locations
-cp $BASH_DIR/fixtures/saml20-idp-hosted.php  $PREPARE_DIR/private/simplesamlphp/metadata/saml20-idp-hosted.php
-cp $BASH_DIR/fixtures/shib13-idp-hosted.php  $PREPARE_DIR/private/simplesamlphp/metadata/shib13-idp-hosted.php
-cp $BASH_DIR/fixtures/saml20-sp-remote.php  $PREPARE_DIR/private/simplesamlphp/metadata/saml20-sp-remote.php
-cp $BASH_DIR/fixtures/shib13-sp-remote.php  $PREPARE_DIR/private/simplesamlphp/metadata/shib13-sp-remote.php
+cp "$BASH_DIR"/fixtures/saml20-idp-hosted.php  "$PREPARE_DIR"/private/simplesamlphp/metadata/saml20-idp-hosted.php
+cp "$BASH_DIR"/fixtures/shib13-idp-hosted.php  "$PREPARE_DIR"/private/simplesamlphp/metadata/shib13-idp-hosted.php
+cp "$BASH_DIR"/fixtures/saml20-sp-remote.php  "$PREPARE_DIR"/private/simplesamlphp/metadata/saml20-sp-remote.php
+cp "$BASH_DIR"/fixtures/shib13-sp-remote.php  "$PREPARE_DIR"/private/simplesamlphp/metadata/shib13-sp-remote.php
 
 # Enable the exampleauth module
-touch $PREPARE_DIR/private/simplesamlphp/modules/exampleauth/enable
+touch "$PREPARE_DIR"/private/simplesamlphp/modules/exampleauth/enable
 
 # Generate a certificate SimpleSAMLphp uses for encryption
 # Because these files are in ~/code/private, they're inaccessible from the web
-openssl req -newkey rsa:2048 -new -x509 -days 3652 -nodes -out $PREPARE_DIR/private/simplesamlphp/cert/saml.crt -keyout $PREPARE_DIR/private/simplesamlphp/cert/saml.pem -batch
+openssl req -newkey rsa:2048 -new -x509 -days 3652 -nodes -out "$PREPARE_DIR"/private/simplesamlphp/cert/saml.crt -keyout "$PREPARE_DIR"/private/simplesamlphp/cert/saml.pem -batch
 
 # Modify the login template so Behat can submit the form
-sed -i  -- "s/<button/<button id='submit'/g" $PREPARE_DIR/private/simplesamlphp/modules/core/templates/loginuserpass.tpl.php
-sed -i  -- "s/this.disabled=true; this.form.submit(); return true;//g" $PREPARE_DIR/private/simplesamlphp/modules/core/templates/loginuserpass.tpl.php
+sed -i  -- "s/<button/<button id='submit'/g" "$PREPARE_DIR"/private/simplesamlphp/modules/core/templates/loginuserpass.tpl.php
+sed -i  -- "s/this.disabled=true; this.form.submit(); return true;//g" "$PREPARE_DIR"/private/simplesamlphp/modules/core/templates/loginuserpass.tpl.php
 # Second button instance shouldn't have an id
-sed -i  -- "s/<button id='submit' class=\"btn\" tabindex=\"6\"/<button class=\"btn\" tabindex=\"6\"/g" $PREPARE_DIR/private/simplesamlphp/modules/core/templates/loginuserpass.tpl.php
+sed -i  -- "s/<button id='submit' class=\"btn\" tabindex=\"6\"/<button class=\"btn\" tabindex=\"6\"/g" "$PREPARE_DIR"/private/simplesamlphp/modules/core/templates/loginuserpass.tpl.php
 
-cd $PREPARE_DIR
+cd "$PREPARE_DIR"
 # Make the SimpleSAMLphp installation publicly accessible
 ln -s ./private/simplesamlphp/www ./simplesaml
 
 ###
 # Push files to the environment
 ###
-cd $PREPARE_DIR
 git add private wp-content simplesaml
 git config user.email "wp-saml-auth@getpantheon.com"
 git config user.name "Pantheon"
@@ -130,26 +129,26 @@ git commit -m "Include SimpleSAMLphp and its configuration files"
 git push
 
 # Sometimes Pantheon takes a little time to refresh the filesystem
-terminus build:workflow:wait $TERMINUS_SITE.$TERMINUS_ENV
+terminus build:workflow:wait "$SITE_ENV"
 
 ###
 # Copy the Pantheon.yml to switch PHP to 7.4
 ###
-cp $BASH_DIR/fixtures/pantheon.php74.yml $PREPARE_DIR/pantheon.yml
+cp "$BASH_DIR"/fixtures/pantheon.php74.yml "$PREPARE_DIR"/pantheon.yml
 git add pantheon.yml
 git commit -m "Set PHP version to 7.4"
 git push
-terminus build:workflow:wait $TERMINUS_SITE.$TERMINUS_ENV
+terminus build:workflow:wait "$SITE_ENV"
 
 ###
 # Set up WordPress, theme, and plugins for the test run
 ###
 # Silence output so as not to show the password.
 {
-  terminus wp $SITE_ENV -- core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=$WORDPRESS_ADMIN_USERNAME --admin_email=$WORDPRESS_ADMIN_EMAIL --admin_password=$WORDPRESS_ADMIN_PASSWORD
+  terminus wp "$SITE_ENV" -- core install --title="${TERMINUS_ENV}-${TERMINUS_SITE}" --url="$PANTHEON_SITE_URL" --admin_user="$WORDPRESS_ADMIN_USERNAME" --admin_email="$WORDPRESS_ADMIN_EMAIL" --admin_password="$WORDPRESS_ADMIN_PASSWORD"
 } &> /dev/null
-terminus wp $SITE_ENV -- option update home "https://$PANTHEON_SITE_URL"
-terminus wp $SITE_ENV -- option update siteurl "https://$PANTHEON_SITE_URL"
-terminus wp $SITE_ENV -- plugin activate wp-native-php-sessions wp-saml-auth
-terminus wp $SITE_ENV -- theme activate $TERMINUS_SITE
-terminus wp $SITE_ENV -- rewrite structure '/%year%/%monthnum%/%day%/%postname%/'
+terminus wp "$SITE_ENV" -- option update home "https://$PANTHEON_SITE_URL"
+terminus wp "$SITE_ENV" -- option update siteurl "https://$PANTHEON_SITE_URL"
+terminus wp "$SITE_ENV" -- plugin activate wp-native-php-sessions wp-saml-auth
+terminus wp "$SITE_ENV" -- theme activate "$TERMINUS_SITE"
+terminus wp "$SITE_ENV" -- rewrite structure '/%year%/%monthnum%/%day%/%postname%/'
---
+++ bin/fixtures/functions.simplesaml1.18.0.php
@@ -0,0 +1,36 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'samltheme_parent_theme_enqueue_styles' );
+
+function samltheme_parent_theme_enqueue_styles() {
+    wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'samltheme-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'theme-style' )
+    );
+
+}
+
+add_filter( 'wp_saml_auth_option', function( $value, $option_name ){
+    if ( 'connection_type' === $option_name ) {
+        return 'internal';
+    }
+    if ( 'internal_config' === $option_name ) {
+        $value['idp']['entityId'] = home_url( '/simplesaml/saml2/idp/metadata.php' );
+        $value['idp']['singleSignOnService']['url'] = home_url( '/simplesaml/saml2/idp/SSOService.php' );
+        $value['idp']['x509cert'] = file_get_contents( ABSPATH . '/private/simplesamlphp/cert/saml.crt' );
+        $value['idp']['singleLogoutService']['url'] = home_url( '/simplesaml/saml2/idp/SingleLogoutService.php' );
+        return $value;
+    }
+    // From https://commons.lbl.gov/display/IDMgmt/Attribute+Definitions#AttributeDefinitions-uiduid
+    if ( 'user_login_attribute' === $option_name ) {
+        return 'urn:oid:0.9.2342.19200300.100.1.1';
+    }
+    if ( 'user_email_attribute' === $option_name ) {
+        return 'urn:oid:0.9.2342.19200300.100.1.3';
+    }
+    if ( 'permit_wp_login' === $option_name ) {
+        return false;
+    }
+    return $value;
+}, 11, 2 );
