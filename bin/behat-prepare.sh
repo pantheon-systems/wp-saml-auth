146adfdd5e65d49837f02d782a21d73e94326bac
Chris Reynolds <chris@jazzsequence.com>
2025-05-28 15:37:55 -0600

    fix shellcheck warnings

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -74,7 +74,7 @@ rsync -av --exclude='node_modules/' --exclude='simplesamlphp/' --exclude='tests/
 rm -rf "$PREPARE_DIR"/wp-content/plugins/wp-saml-auth/.git
 
 # Add extra tests if we're running 2.0.0
-if [ $SIMPLESAMLPHP_VERSION == '2.0.0' ]; then
+if [ "$SIMPLESAMLPHP_VERSION" == '2.0.0' ]; then
 	WORKING_DIR="/home/tester/pantheon-systems/wp-saml-auth"
 	# Check that the WORKING _DIRECTORY exists
 	if [ ! -d "$WORKING_DIR" ]; then
@@ -119,7 +119,7 @@ fi
 echo "Setting up SimpleSAMLphp $SIMPLESAMLPHP_VERSION"
 rm -rf "$PREPARE_DIR"/private
 mkdir "$PREPARE_DIR"/private
-wget $SIMPLESAMLPHP_DOWNLOAD_URL -O "$PREPARE_DIR"/simplesamlphp-latest.tar.gz
+wget "$SIMPLESAMLPHP_DOWNLOAD_URL" -O "$PREPARE_DIR"/simplesamlphp-latest.tar.gz
 tar -zxvf "$PREPARE_DIR"/simplesamlphp-latest.tar.gz -C "$PREPARE_DIR"/private
 ORIG_SIMPLESAMLPHP_DIR=$(ls "$PREPARE_DIR"/private)
 mv "$PREPARE_DIR"/private/"$ORIG_SIMPLESAMLPHP_DIR" "$PREPARE_DIR"/private/simplesamlphp
