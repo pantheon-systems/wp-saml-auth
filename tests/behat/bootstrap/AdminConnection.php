5dcbe1269317a5010b9ed8c0ec655613bfd2b38c
Anais Gueyte <anaisgueyte@pantheon.io>
2025-08-20 11:51:21 -0400

    Update adminconnection comment

--- tests/behat/bootstrap/AdminConnection.php
+++ tests/behat/bootstrap/AdminConnection.php
@@ -5,7 +5,7 @@ namespace PantheonSystems\WPSamlAuth\Behat;
 use Behat\Behat\Context\Context;
 
 /**
- * This class replaces PantheonSystems\PantheonWordPressUpstreamTests\Behat\AdminLogIn
+ * This class replaces PantheonSystems\PantheonWordPressUpstreamTests\Behat\AdminConnection
  * However it does not actually extend it so as to avoid unecessary coupling.
  */
 class AdminConnection implements Context {
