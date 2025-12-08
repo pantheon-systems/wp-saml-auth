2cff11012c8289cec29e6fd394ae08369e8807b5
Anais Gueyte <anaisgueyte@pantheon.io>
2025-12-08 16:20:36 -0500

    add admin login feature

--- tests/behat/bootstrap/AdminLogIn.php
+++ tests/behat/bootstrap/AdminLogIn.php
@@ -39,6 +39,16 @@ class AdminLogIn implements Context, SnippetAcceptingContext {
         $this->minkContext->assertPageAddress("wp-admin/");
     }
 
+    /**
+     * @Given my connection type is :arg1
+     */
+    public function myConnectionTypeIs($arg1)
+    {
+    	// checks the connection type for adminNotice behat test
+    	global $connection_type;
+    	$connection_type = $arg1;
+	}
+
 	/**
 	 * @Then I follow the SAML redirect manually
 	 */
