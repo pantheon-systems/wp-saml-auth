3b559b0eee717fa7d3da000cf629176f54624855
Asim Mehta <asimmehta@pantheon.io>
2026-08-12 11:56:18 -0400

    Release 2.3.4

--- wp-saml-auth.php
+++ wp-saml-auth.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP SAML Auth
- * Version: 2.3.4-dev
+ * Version: 2.3.4
  * Description: SAML authentication for WordPress, using SimpleSAMLphp.
  * Author: Pantheon
  * Author URI: https://pantheon.io
